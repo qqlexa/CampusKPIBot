34726625f716349fa05ce3a71cb54dcf2acdfbd1
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-06-20 23:03:05 +0300

    Add files via upload
    Added saving stats
    Added multithreading

--- campus_parser.py
+++ campus_parser.py
@@ -2,103 +2,156 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
-import asyncio
 import time
 
-import ujson
-import asyncio
-from aiohttp import web
-
-import pandas as pd
-import numpy as np
-from openpyxl import load_workbook
+from threading import Thread
 
 
 def try_use(warning_message="Unknown error", warning_func=None):
     def actual_decorator(func):
-        async def wrapper(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             try:
-                await func(*args, **kwargs)
+                func(*args, **kwargs)
             except BaseException:
                 print(warning_message)
-                if warning_func:
-                    warning_func()
-                return False
+                if not warning_func:
+                    return warning_message
+                warning_func()
             else:
                 return True
         return wrapper
     return actual_decorator
 
 
-"""
-def save_info(params, marks, startrow=1):
-    df_list = list()
-
-    cols_names = ['Faculty', 'Group', 'Studying form', 'Course', 'Speciality', 'Studying status']
-
-    for i in range(len(params)):
-        cur_list = list()
-        cur_list.append(params[i])
-        df_list.append(pd.DataFrame({cols_names[i]: cur_list}))
-
-    for i in range(len(marks)):
-        cur_list = list()
-        cur_list.append(marks[i])
-        df_list.append(pd.DataFrame({"": cur_list}))
-
-    writer = pd.ExcelWriter('stat.xlsx', engine='openpyxl')
-    try:
-        book = load_workbook("stat.xlsx")
-    except BaseException:
-        pass
-    else:
-        writer.book = book
-        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-
-    i = 0
-    for df in df_list:
-        df.to_excel(writer, sheet_name='new_df', startcol=i, startrow=startrow * 2, index=False)
-        i += 1
-
-    writer.save()
-"""
-
-
-class Campus:
-
-    def __init__(self):
-        print("Campus obj is creating")
-        self.options = webdriver.ChromeOptions()
-        self.options.add_argument('headless')
-
-        self.timeout = 1
-
+class MarksThread(Thread):
+    def __init__(self, student_account):
+        Thread.__init__(self)
         self.driver = None
         self.driver_status = True
 
-        self.auth_info = list()
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('headless')
+
+        self.student_account = student_account
+
+        self.success = False
+        self.status = False
 
         self.login_url = "https://ecampus.kpi.ua/"
         self.stat_url = "https://campus.kpi.ua/student/index.php?mode=profile"
         self.marks_url = "https://campus.kpi.ua/student/index.php?mode=vedomoststud"
+
+        self.timeout = 3
+
+        self._return = None
+
+    def run(self):
+        try:
+            self.driver = webdriver.Chrome(chrome_options=self.options)
+
+            self.driver.get(self.login_url)
+
+            auth_fields = self.driver.find_elements_by_class_name("form-control")
+
+            login_field = auth_fields[0]
+            password_field = auth_fields[1]
+
+            login = self.student_account[0]
+            password = self.student_account[1]
+
+            login_field.send_keys(login)
+            password_field.send_keys(password)
+
+            answer = self.click_login()
+            if isinstance(answer, str):
+                self.close()
+                self._return = answer
+                return
+
+            # success
+            time.sleep(self.timeout)
+
+            answer = self.auth_account()
+            if isinstance(answer, str):
+                self.close()
+                self._return = answer
+                return
+
+            time.sleep(self.timeout)
+
+            self.driver.get(self.marks_url)
+
+            time.sleep(self.timeout)
+
+            name = self.driver.find_elements_by_class_name("title")[0].text[9::][:-1]
+
+            table_id = self.driver.find_elements_by_class_name("cntnt")[0]
+            rows = table_id.find_elements_by_tag_name("tr")[7::][:-1]
+
+            marks = list()
+            for row in rows:
+                cols = row.find_elements_by_tag_name("td")
+                marks.append(int(cols[-2].text))
+
+            self.driver.get(self.stat_url)
+
+            time.sleep(self.timeout)
+
+            table_id = self.driver.find_elements_by_class_name("cntnt")[0]
+            rows = table_id.find_elements_by_tag_name("tr")[4::][:-6]
+
+            faculty = rows[0].find_elements_by_tag_name("td")[0].text
+            group = rows[1].find_elements_by_tag_name("td")[0].text
+            form_studying = rows[3].find_elements_by_tag_name("td")[0].text
+            course_studying = rows[4].find_elements_by_tag_name("td")[0].text
+            speciality = rows[5].find_elements_by_tag_name("td")[0].text
+            is_studying = rows[6].find_elements_by_tag_name("td")[0].text
+
+            self.close()
+        except BaseException:
+            self.close()
+            self._return = list(), list()
+        else:
+            info = [name, faculty, group, form_studying, course_studying, speciality, is_studying]
+            self._return = info, marks
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+    def get_return(self):
+        return self._return
+
+    @try_use("Problem with clicking")
+    def click_login(self):
+        button_submit = self.driver.find_elements_by_class_name("btn-block")[0]
+        button_submit.click()
+
+    @try_use("Problem with authorization")
+    def auth_account(self):
+        button_login = self.driver.find_elements_by_class_name("btn-lg")[0]
+        button_login.click()
 
     def close(self):
         if self.driver_status:
             self.driver.close()
             self.driver_status = False
 
-    def page_has_loaded(self):
-        page_state = self.driver.execute_script('return document.readyState;')
-        return page_state == 'complete'
+
+class Campus:
+
+    def __init__(self):
+        print("Campus obj is creating")
+        self.auth_info = list()
 
     @try_use("self.auth_info is broken")
-    async def print_logs_passwords(self):
+    def print_logs_passwords(self):
         for i in self.auth_info:
             print(i)
         return True
 
     @try_use("self.add_logs_passwords() was crashed")
-    async def add_logs_passwords(self, path="E:\\STUDYING\\pass.txt"):
+    def add_logs_passwords(self, path="E:\\STUDYING\\pass.txt"):
         """
         :append: auth_info : list<list>
         [0] - login
@@ -114,111 +167,16 @@ class Campus:
         return True
 
     @try_use("self.add_log_password() was crashed")
-    async def add_log_password(self, student_data):
+    def add_log_password(self, student_data):
         self.auth_info.append(student_data)
         return True
 
-    @try_use("Problem with clicking")
-    async def click_login(self):
-        button_submit = self.driver.find_elements_by_class_name("btn-block")[0]
-        button_submit.click()
-        return True
-
-    @try_use("Problem with authorization")
-    async def auth_account(self):
-        button_login = self.driver.find_elements_by_class_name("btn-lg")[0]
-        button_login.click()
-        return True
-
-    async def get_campus_marks(self, student_account, startrow=0):
-        try:
-
-            self.driver = webdriver.Chrome(chrome_options=self.options)
-            self.driver_status = True
-
-            self.driver.get(self.login_url)
-
-            auth_fields = self.driver.find_elements_by_class_name("form-control")
-
-            login_field = auth_fields[0]
-            password_field = auth_fields[1]
-
-            print(student_account)
-
-            login = student_account[0]
-            password = student_account[1]
-
-            login_field.send_keys(login)
-            password_field.send_keys(password)
-
-            if not await self.click_login():
-                self.close()
-                return False
-
-            # success
-            await asyncio.sleep(3)
-
-            if not await self.auth_account():
-                self.close()
-                return False
-
-            await asyncio.sleep(3)
-
-            self.driver.get(self.marks_url)
-
-            await asyncio.sleep(3)
-
-            name = self.driver.find_elements_by_class_name("title")[0].text[9::][:-1]
-            print(name)
-
-            table_id = self.driver.find_elements_by_class_name("cntnt")[0]
-            rows = table_id.find_elements_by_tag_name("tr")[7::][:-1]
-
-            marks = list()
-            for row in rows:
-                try:
-                    cols = row.find_elements_by_tag_name("td")
-                except BaseException:
-                    pass
-                else:
-                    try:
-                        marks.append(int(cols[-2].text))
-                    except BaseException:
-                        pass
-
-            self.driver.get(self.stat_url)
-
-            await asyncio.sleep(3.5)
-
-            table_id = self.driver.find_elements_by_class_name("cntnt")[0]
-            rows = table_id.find_elements_by_tag_name("tr")[4::][:-6]
-
-            faculty = rows[0].find_elements_by_tag_name("td")[0].text
-            group = rows[1].find_elements_by_tag_name("td")[0].text
-            form_studying = rows[3].find_elements_by_tag_name("td")[0].text
-            course_studying = rows[4].find_elements_by_tag_name("td")[0].text
-            speciality = rows[5].find_elements_by_tag_name("td")[0].text
-            is_studying = rows[6].find_elements_by_tag_name("td")[0].text
-
-            self.close()
-
-        except BaseException:
-            self.close()
-        else:
-            return [faculty, group, form_studying, course_studying, speciality, is_studying], marks
-
-        return False
-
-
-async def main():
-    campus = Campus()
-    await campus.add_logs_passwords()
-
-    startrow = 0
-    for student_account in campus.auth_info:
-        await campus.get_campus_marks(student_account, startrow)
-        startrow += 1
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    @staticmethod
+    def get_campus_marks(student_account):
+        thread = MarksThread(student_account)
+        thread.start()
+        if thread.join():
+            if isinstance(thread.get_return(), str):
+                return list(), list(), thread.get_return()
+            return thread.get_return()[0], thread.get_return()[1], True
+        return list(), list(), False
--- telegram_bot.py
+++ telegram_bot.py
@@ -6,68 +6,221 @@ from telegram.ext import MessageHandler, Filters
 import campus_parser
 import asyncio
 
+from datetime import datetime
+from threading import Thread
 
-class DialogBot(object):
+import time
+
+import saver
+
+
+class Queue:
+    def __init__(self):
+        """
+        [
+            user_id : int
+            student_account : list (login, password)
+        ],
+        """
+        self.queue = list()
+
+    @property
+    def len(self):
+        return len(self.queue)
+
+    def get_position(self, position=0):
+        return self.queue[position]
+
+    def delete_position(self, position=0):
+        return self.queue.pop(position)
+
+    def add_to_queue(self, queue_id, student_account, root=False):
+        if root:
+            self.queue.append([queue_id, student_account])
+            return
+
+        if not self.is_in_queue(queue_id):
+            self.queue.append([queue_id, student_account])
+            return "You added to queue"
+        else:
+            return "You are already in queue"
+
+    def is_in_queue(self, queue_id):
+        for i in self.queue:
+            if i[0] == queue_id:
+                return True
+        return False
+
+
+class PersonalThread(Thread):
+    def __init__(self, telegram_bot):
+        Thread.__init__(self)
+        self.telegram_bot = telegram_bot
+
+    def run(self):
+        while True:
+            while self.telegram_bot.queue.len:
+                position = self.telegram_bot.queue.get_position()
+                print(position)
+                info, marks, message = self.telegram_bot.campus.get_campus_marks([position[1][0], position[1][1]])
+                if not isinstance(message, str):
+                    if len(info):
+                        message = ""
+                        for i in info:
+                            message += i + "\n"
+                        message += "\n"
+                        for mark in marks:
+                            message += str(mark) + "\n"
+
+                        average_mark = round(sum([int(mark) for mark in marks]) / len(marks), 2)
+
+                        message += "\nAverage: "
+                        message += str(average_mark)
+
+                if isinstance(message, str):
+                    self.telegram_bot.context.bot.send_message(chat_id=position[0], text=message)
+                self.telegram_bot.queue.delete_position()
+
+
+class TelegramThread(Thread):
+    def __init__(self, telegram_bot):
+        Thread.__init__(self)
+        self.telegram_bot = telegram_bot
+        self.info_list = list()
+
+    def sort_info(self):
+        for i in range(len(self.info_list)):
+            for j in range(0, len(self.info_list) - 1):
+                try:
+                    if self.info_list[j][1] < self.info_list[j + 1][1]:
+                        self.info_list[j], self.info_list[j + 1] = self.info_list[j + 1], self.info_list[j]
+                except BaseException:
+                    pass
+                    # print("Error sorting")
+
+    def count_students_speciality(self, speciality):
+        count = 0
+        for info in self.info_list:
+            if info[0][5] == speciality:
+                count += 1
+        return count
+
+    def run(self):
+        while self.telegram_bot.all_queue.len:
+            position = self.telegram_bot.all_queue.get_position()
+            print(position)
+            info, marks, message = self.telegram_bot.campus.get_campus_marks([position[1][0], position[1][1]])
+            if not isinstance(message, str):
+                if len(info):
+                    message = ""
+                    for i in info:
+                        message += i + "\n"
+                    message += "\n"
+                    for mark in marks:
+                        message += str(mark) + "\n"
+                    average_mark = round(sum([int(mark) for mark in marks]) / len(marks), 2)
+
+                    message += "\nAverage: "
+                    message += str(average_mark)
+                    print(average_mark)
+                    self.info_list.append([info, average_mark])
+
+            if isinstance(message, str):
+                self.telegram_bot.context.bot.send_message(chat_id=position[0], text=message)
+            try:
+                self.telegram_bot.all_queue.delete_position()
+            except BaseException:
+                pass
+
+        # sort_info
+        self.sort_info()
+
+        # save
+        keeper = saver.Saver()
+        for info in self.info_list:
+            keeper.save_info([info[0], info[1]], len(info[0]))
+
+        self.telegram_bot.thread_status = False
+
+
+class TelegramBot(object):
 
     def __init__(self, token):
         self.updater = Updater(token, use_context=True)
 
         self.updater.dispatcher.add_handler(CommandHandler('start', self.start_command))
+        self.updater.dispatcher.add_handler(CommandHandler('help', self.help_command))
         self.updater.dispatcher.add_handler(CommandHandler('login', self.login_command))
+        self.updater.dispatcher.add_handler(CommandHandler('logins', self.logins_command))
 
         self.updater.dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), self.echo))
 
         self.campus = campus_parser.Campus()
+        self.queue = Queue()
+        self.all_queue = Queue()
+
+        self.context = None
+        self.personal_status = False
+        self.thread_status = False
+
+        self.qqlexa_id = 440973597
 
     def start(self):
         self.updater.start_polling()
 
     @staticmethod
-    def start_command(context, update):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    def start_command(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="I'm a bot for getting information about your campus account")
 
     @staticmethod
-    def help_command(context, update):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    def help_command(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="/login $login $password")
 
     @staticmethod
     def echo(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+        delta = datetime.utcnow().second - update.message["date"].second
+        if abs(delta) < 2:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=update.message.text)
 
     def login_command(self, update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Hi")
-        print("Hi")
-        print(update.message.text)
-        info = update.message.text.split()
-        if len(info) == 3:
-            print("OKAY")
-            asyncio.run(self.get_marks([info[1], info[2]], update, context))
+        self.context = context
+        delta = datetime.utcnow().second - update.message["date"].second
+        if abs(delta) < 2:
+            info = update.message.text.split()
+            if len(info) == 3:
+                add_answer = self.queue.add_to_queue(update.message.from_user.id, [info[1], info[2]])
 
-    async def get_marks(self, student_account, update, context):
-        info, marks = await self.campus.get_campus_marks(student_account)
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=add_answer)
+                if not self.personal_status:
+                    self.personal_status = True
+                    thread = PersonalThread(self)
+                    thread.start()
+            else:
+                self.help_command(context, update)
 
-        faculty = info[0]
-        group = info[1]
-        form_studying = info[2]
-        course_studying = info[3]
-        speciality = info[4]
-        is_studying = info[5]
+    def logins_command(self, update, context):
+        self.context = context
+        delta = datetime.utcnow().second - update.message["date"].second
+        if abs(delta) < 2:
+            if not self.thread_status and update.message.from_user.id == self.qqlexa_id:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="It is running")
 
-        context.bot.send_message(chat_id=update.effective_chat.id, text=faculty)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=group)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=form_studying)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=course_studying)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=speciality)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=is_studying)
+                self.thread_status = True
 
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Your marks:")
+                self.campus.add_logs_passwords(path="E:\\STUDYING\\pass.txt")  # TM 91
+                for student_account in self.campus.auth_info:
+                    self.all_queue.add_to_queue(update.message.from_user.id, student_account, root=True)
 
-        for mark in marks:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=mark)
+                thread = TelegramThread(self)
+                thread.start()
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="It is already working")
 
 
 if __name__ == '__main__':
-    bot = DialogBot(TOKEN)
+    bot = TelegramBot('1283008038:AAGWriN3_a6ibRVkZuiZjehgL-rFLVkgDh8')
     bot.start()
-
-
